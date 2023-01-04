84033373d4ac1ff89591a02009a08e1abf1abdfc
Vishota <88852731+Vishota@users.noreply.github.com>
2023-01-04 21:33:41 +0300

    custom input

---
+++ lighning-protection/src/components/ValueInput.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+    var props = defineProps(['modelValue', 'type', 'label'])
+</script>
+<template>
+    <div>
+        <div class="container">
+            <p>{{ label }}:</p>
+            <input :type="type == undefined ? 'text' : type" placeholder="test" :value="modelValue" @input="$emit('update:modelValue', ($event as any).target.value)">
+        </div>
+    </div>
+</template>
+<style scoped>
+    .container {
+        display: grid;
+        grid-template-columns: 1fr 3fr;
+    }
+    input, p {
+        width: 100%;
+    }
+</style>
